5bbf53c0ffc687c4e0de2066983894eed4822b97
Achi <smirnov-clavik@yandex.ru>
2024-09-17 18:04:50 +0300

    Create module_1_5.py

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = ["Sting", "Box", 1, 2, True]
+print(immutable_var)
+print('Потому что кортежи неизменяемы. Если вам нужны изменяемые последовательности, используйте списки')
+mutable_list = ['Ball', 'Kit', True]
+mutable_list[0] = 'Knife'
+print(mutable_list)
