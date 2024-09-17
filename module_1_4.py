0a25e84ba54cf47f48bc680085b8e1861a0f4ed7
Achi <smirnov-clavik@yandex.ru>
2024-09-17 18:04:15 +0300

    Create module_1_4.py

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input("Привет, больше не пиши сюда: ")
+print(len('Привет, больше не пиши сюда: '))
+print('my_string'.upper())
+print('my_string'.lower())
+print('Привет, больше не пиши сюда: '.replace(' ','#'))
+print('my_string' [0])
+print('my_string' [-1])
